66f87a8cd9c5bdb4487e8fdf549664f453bd23fa
Profile <devpage@126.com>
2024-01-05 17:38:38 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,189 +1,130 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
 import streamlit as st
-import google.generativeai as genai
-import dataclasses
-from PIL import Image
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationEntityMemory
+from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
+from langchain.llms import OpenAI
 
-def clear_btn():
-    del st.session_state.messages
-    del st.session_state.image
-    
-@dataclasses.dataclass
-class GenerationConfig:
-    max_output_tokens: int
-    temperature: float
-    top_p: float
-    top_k: int
+st.set_page_config(page_title='Lite🤖', layout='wide')
 
-def set_config():
-    model_config = {}
-    model_image = {'model_name':'', 'image':None,'chat_turn':None,'safety_settings':{}}
-    with st.sidebar:
-        model_name = st.selectbox(
-            'Select gemini model:',
-            ['Gemini Pro', 'Gemini Pro Vision'],
-            index = 0
-        )
-        if model_name=='Gemini Pro':
-            chat_turn = st.selectbox(
-                'Select conversation turn',
-                ('Single-turn Conversation', 'Multi-turn Conversation'),
-                index=0
-            )
-            model_image['chat_turn'] = chat_turn
-        with st.expander("Safety settings"):
-            harassment = st.selectbox(
-                "Harassment", 
-                ['unspecified', 'low', 'medium', 'high', 'block_none'],
-                index = 2
-            )
-            hate = st.selectbox(
-                "Hate Speech",
-                ['unspecified', 'low', 'medium', 'high', 'block_none'],
-                index = 2
-            )
-            sexual = st.selectbox(
-                "Sexually Explicit", 
-                ['unspecified', 'low', 'medium', 'high', 'block_none'],
-                index = 2
-            )
-            danger = st.selectbox(
-                "Dangerous Content", 
-                ['unspecified', 'low', 'medium', 'high', 'block_none'],
-                index = 2
-            )
-        
-        output_len = st.slider(
-            'Output Length',0, 4096, 1024,step=1
-        )
-        temperature = st.slider(
-            'Temperature', 0.0, 2.0, 0.8, step=0.01
-        )
-        top_p = st.slider(
-            'Top P', 0.0, 1.0, 0.9, step=0.01
-        )
-        top_k = st.slider(
-            'Top K', 0, 64, 16, step=1
-        )
-        st.button("Clear Chat History", on_click=clear_btn)
-        if model_name=='Gemini Pro Vision':
-            image_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
-            model_image['image'] = image_file
-            
-    model_image['model_name'] = model_name
-    model_image['safety_settings']['harassment'] = harassment
-    model_image['safety_settings']['hate'] = hate
-    model_image['safety_settings']['sexual'] = sexual
-    model_image['safety_settings']['danger'] = danger
-    model_config['max_output_tokens'] = output_len
-    model_config['temperature'] = temperature
-    model_config['top_p'] = top_p
-    model_config['top_k'] = top_k
-    
-    return model_config,model_image
-    
-def chat(query,model_name,model_config,api_key,safety_settings,history=None):
-    genai.configure(api_key=api_key)
-    
-    model = genai.GenerativeModel(model_name=model_name,generation_config=genai.types.GenerationConfig(model_config))
-    if history is not None:
-        history.append({'role':'user','parts':[query]})
-        response = model.generate_content(history, safety_settings=safety_settings)
-    else:
-        response = model.generate_content(query,safety_settings=safety_settings)
+# OPEN_AI_URL = st.secrets["open_ai_url"]
+OPEN_AI_KEY = st.secrets["open_ai_key"]
+# Initialize session states
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+if "input" not in st.session_state:
+    st.session_state["input"] = ""
+if "stored_session" not in st.session_state:
+    st.session_state["stored_session"] = []
 
-    return response.text
-        
-if __name__=="__main__":
-    GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY", type="password")
-    if not GOOGLE_API_KEY:
-        st.info("Please add your key to continue.")
-        st.stop()
-    
-    genai.configure(api_key=GOOGLE_API_KEY)
-    
-    user_avator = "🧑‍💻"
-    robot_avator = "🤖"
-    
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    if 'image' not in st.session_state:
-        st.session_state.image = []
-        
-    model_config, model_image = set_config()
-    model_name = model_image['model_name']
-    image_file = model_image['image']
-    safety_settings = model_image['safety_settings']
-    chat_turn = model_image['chat_turn']
-    if st.session_state.messages and model_name != st.session_state.messages[-1].get("model_name"):
-        st.session_state.messages = []
-    
-    st.header(f'Google: Gemini Model')
-    if model_name=='Gemini Pro':
-        history = []
-        if chat_turn=='Multi-turn Conversation':
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"], avatar=message.get("avatar")):
-                    st.markdown(message["content"])
-                    role = message['role']
-                    if role=='robot':
-                        role = 'model'
-                    history.append({'role':role,'parts':[message["content"]]})
-        else:
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"], avatar=message.get("avatar")):
-                    st.markdown(message["content"])
-        if user_query := st.chat_input("Please enter content......"):
-            with st.chat_message("user", avatar=user_avator):
-                st.markdown(user_query)
-            st.session_state.messages.append({"role": "user", "content": user_query, "avatar": user_avator, "model_name":model_name})
-            with st.chat_message("robot", avatar=robot_avator):
-                message_placeholder = st.empty()
-                if chat_turn=='Single-turn Conversation':
-                    cur_response = chat(query=user_query,model_name=model_name,model_config=model_config,api_key=GOOGLE_API_KEY)
-                elif chat_turn=='Multi-turn Conversation':
-                    history.append({'role':'user','parts':user_query})
-                    cur_response = chat(query=user_query,model_name=model_name,model_config=model_config,api_key=GOOGLE_API_KEY,history=history)
-                message_placeholder.markdown(cur_response)
-            st.session_state.messages.append({"role": "robot", "content": cur_response, "avatar": robot_avator, "model_name":model_name})
-    elif model_name=='Gemini Pro Vision':
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"], avatar=message.get("avatar")):
-                try:
-                    image = Image.open(message["content"])
-                    st.image(image, use_column_width=True)
-                except Exception:
-                    st.markdown(message["content"])
-        if user_query := st.chat_input("Please enter content......"):
-            with st.chat_message("user", avatar=user_avator):
-                if not st.session_state.image and image_file is not None:
-                    st.session_state.image.append(image_file)
-                    image = Image.open(image_file)
-                    st.image(image, use_column_width=True)
-                    st.markdown(user_query)
-                    st.session_state.messages.append({"role": "user", "content": image_file, "avatar": user_avator, "model_name":model_name})
-                    st.session_state.messages.append({"role": "user", "content": user_query, "avatar": user_avator, "model_name":model_name})
-                    
-                elif isinstance(image_file,st.runtime.uploaded_file_manager.UploadedFile) and image_file!=st.session_state.image[-1]:
-                    st.session_state.image.append(image_file)
-                    image_file = st.session_state.image[-1]
-                    image = Image.open(image_file)
-                    st.image(image, use_column_width=True)
-                    st.markdown(user_query)
-                    st.session_state.messages.append({"role": "user", "content": image_file, "avatar": user_avator, "model_name":model_name})
-                    st.session_state.messages.append({"role": "user", "content": user_query, "avatar": user_avator, "model_name":model_name})
-                else:
-                    st.markdown(user_query)
-                    st.session_state.messages.append({"role": "user", "content": user_query, "avatar": user_avator, "model_name":model_name})
-            if st.session_state.messages!=[]:
-                
-                if not isinstance(st.session_state.messages[0]['content'],st.runtime.uploaded_file_manager.UploadedFile):
-                    st.session_state.messages = []
-                    st.error('Add an image to use Gemini Pro Vision, or switch your model to a text model (Gemini Pro)', icon="🚨")
-                else:
-                    with st.chat_message("robot", avatar=robot_avator):
-                        message_placeholder = st.empty()
-                        image_file = st.session_state.image[-1]
-                        image = Image.open(image_file)
-                        cur_response = chat(query=[user_query,image],model_name=model_name,model_config=model_config,api_key=GOOGLE_API_KEY)
-                        message_placeholder.markdown(cur_response)
-                    st.session_state.messages.append({"role": "robot", "content": cur_response, "avatar": robot_avator,"model_name":model_name})
+# Define function to get user input
+def get_text():
+    """
+    Get the user input text.
+
+    Returns:
+        (str): The text entered by the user
+    """
+    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+                            placeholder="Your AI assistant here! Ask me anything ...",
+                            label_visibility='hidden')
+    return input_text
+
+# Define function to start a new chat
+def new_chat():
+    """
+    Clears session state and starts a new chat.
+    """
+    save = []
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        save.append("User:" + st.session_state["past"][i])
+        save.append("Bot:" + st.session_state["generated"][i])
+    st.session_state["stored_session"].append(save)
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["input"] = ""
+    st.session_state.entity_memory.entity_store = {}
+    st.session_state.entity_memory.buffer.clear()
+
+# Set up sidebar with various options
+with st.sidebar.expander("🛠️ ", expanded=False):
+    # Option to preview memory store
+    if st.checkbox("Preview memory store"):
+        with st.expander("Memory-Store", expanded=False):
+            st.session_state.entity_memory.store
+    # Option to preview memory buffer
+    if st.checkbox("Preview memory buffer"):
+        with st.expander("Bufffer-Store", expanded=False):
+            st.session_state.entity_memory.buffer
+    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
+    K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+
+# Set up the Streamlit app layout
+st.title("🤖 Chat Bot with 🧠")
+st.subheader(" Powered by 🦜 LangChain + OpenAI + Streamlit")
+
+# Ask the user to enter their OpenAI API key
+API_O = OPEN_AI_KEY
+
+# Session state storage would be ideal
+if API_O:
+    # Create an OpenAI instance
+    llm = OpenAI(temperature=0,
+                 openai_api_key=API_O,
+                 model_name=MODEL,
+                 verbose=False)
+
+    # Create a ConversationEntityMemory object if not already created
+    if 'entity_memory' not in st.session_state:
+        st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K)
+
+    # Create the ConversationChain object with the specified configuration
+    Conversation = ConversationChain(
+        llm=llm,
+        prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+        memory=st.session_state.entity_memory
+    )
+else:
+    st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
+    # st.stop()
+
+# Add a button to start a new chat
+st.sidebar.button("New Chat", on_click=new_chat, type='primary')
+
+# Get the user input
+user_input = get_text()
+
+# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
+if user_input:
+    output = Conversation.run(input=user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+# Allow to download as well
+download_str = []
+# Display the conversation history using an expander, and allow the user to download it
+with st.expander("Conversation", expanded=True):
+    for i in range(len(st.session_state['generated']) - 1, -1, -1):
+        st.info(st.session_state["past"][i], icon="🧐")
+        st.success(st.session_state["generated"][i], icon="🤖")
+        download_str.append(st.session_state["past"][i])
+        download_str.append(st.session_state["generated"][i])
+
+    # Can throw error - requires fix
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.download_button('Download', download_str)
+
+# Display stored conversation sessions in the sidebar
+for i, sublist in enumerate(st.session_state.stored_session):
+    with st.sidebar.expander(label=f"Conversation-Session:{i}"):
+        st.write(sublist)
+
+# Allow the user to clear all stored conversation sessions
+if st.session_state.stored_session:
+    if st.sidebar.checkbox("Clear-all"):
+        del st.session_state.stored_session
